29722c53af31004335d71d4d331c5dfc3b5e43de
Gerard Burns <gjburns9@hotmail.com>
2021-06-29 10:58:49 +0100

    fix goal tallying

--- src/data/index.js
+++ src/data/index.js
@@ -9,27 +9,36 @@ export const finishedMatchesCount = finishedMatches.length;
 export const totalMatchesCount = goalData.matches.length;
 
 export const goalsScored = finishedMatches.map(match => {
-  if (
-    match.score?.extraTime?.homeTeam !== null &&
-    match.score?.extraTime?.awayTeam !== null
-  ) {
-    return match.score.extraTime.homeTeam + match.score.extraTime.awayTeam;
-  }
-  else if (
-    match.score?.fullTime?.homeTeam !== null &&
-    match.score?.fullTime?.awayTeam !== null
-  ) {
-    return match.score.fullTime.homeTeam + match.score.fullTime.awayTeam;
-  }
-  else if (
-    match.score?.halfTime?.homeTeam !== null &&
-    match.score?.halfTime?.awayTeam !== null
-  ) {
-    return match.score.halfTime.homeTeam + match.score.halfTime.awayTeam;
-  }
-  else {
-    return 0;
-  }
+  const baseScore = () => {
+    if (
+      match.score?.fullTime?.homeTeam !== null &&
+      match.score?.fullTime?.awayTeam !== null
+    ) {
+      return match.score.fullTime.homeTeam + match.score.fullTime.awayTeam;
+    }
+    else if (
+      match.score?.halfTime?.homeTeam !== null &&
+      match.score?.halfTime?.awayTeam !== null
+    ) {
+      return match.score.halfTime.homeTeam + match.score.halfTime.awayTeam;
+    }
+    else {
+      return 0;
+    }
+  };
+
+  const penaltiesScore = () => {
+    if (
+      match.score?.penalties?.homeTeam !== null &&
+      match.score?.penalties?.awayTeam !== null
+    ) {
+      return match.score.penalties.homeTeam + match.score.penalties.awayTeam;
+    } else {
+      return 0;
+    }
+ }
+
+  return baseScore() - penaltiesScore();
 }).reduce((count, acc) => count + acc)
 
 export const averageGoalsPerGame = (goalsScored / finishedMatchesCount).toFixed(2);
